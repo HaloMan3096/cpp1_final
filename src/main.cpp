d6691da2901eace24b1bdb2b2bf0a4353afd92e4
logan <Loganold@hotmail.com>
2024-11-28 16:35:34 -0500

    Did HW fixed a bug?

--- src/main.cpp
+++ src/main.cpp
@@ -24,8 +24,39 @@ class UserSolver : public Solver {
 
 void UserSolver::solve(Board &board) {
     // Your Implementation goes here
-    ReturnType t = guess(1, 1, board);
-    std::cout << t << std::endl;
+    int counter = 0, nearCount = 0, hitCount = 0;
+    for (size_t i = 0; i < board.getX(); i += 3) {
+        for (size_t j = 0; j < board.getY(); j++) {
+            ReturnType t = guess(i, j, board);
+            switch (t) {
+                case bad_guess: // Bad Guess (Out of Bounds)
+                    throw std::invalid_argument("Out of bounds");
+                case hit:
+                    std::cout << "Hit at X: " << i << " Y: " << j << std::endl;
+                hitCount++;
+                break;
+                case miss:
+                    counter++;
+                break;
+                case near_miss: // (One is nearby)
+                    nearCount++;
+                auto g1 = guess(i + 1, j, board);
+                auto g2 = guess(i - 1, j, board);
+                if (g1 == hit) {
+                    std::cout << "Hit From right near miss at X: " << i + 1 << " Y: " << j << std::endl;
+                    hitCount++;
+                }
+                if (g2 == hit) {
+                    std::cout << "Hit From left near miss at X: " << i - 1 << " Y: " << j << std::endl;
+                    hitCount++;
+                }
+                break;
+            }
+        }
+    }
+    std::cout << "Hit: " << hitCount << std::endl;
+    std::cout << "Miss: " << counter << std::endl;
+    std::cout << "Near Miss: " << nearCount << std::endl;
 }
 
 int main() {
@@ -37,7 +68,7 @@ int main() {
     UserSolver us;
     us.solve(b);
 
-    if (es.score() > us.score()) {
+    if (es.score() < us.score()) {
         throw std::runtime_error("Not Efficient Enough");
     }
 }
